6bc2406f175773e5930c8ec405762894bd6ac47b
raulgomezg5 <gomezgarciar4@alumnos.ilerna.com>
2025-10-20 08:51:36 +0200

    Agregar comprobación de edad

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
-nombre = input("¿Cuál es tu nombre? ")
-print(f"Hola, {nombre.upper()}")
+edad = int(input("Introduce tu edad: "))
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")
