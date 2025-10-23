b409ae69aa97962f06cce40b73521aab25ba8da1
raulgomezg5 <gomezgarciar4@alumnos.ilerna.com>
2025-10-23 13:36:17 +0200

    Añadir mi nombre en app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+edad = int(input("Introduce tu edad: "))
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")
+
+print("Hola")
+print("Nueva línea sin commit")
+
+print("Hola, soy Raul Gomez")
