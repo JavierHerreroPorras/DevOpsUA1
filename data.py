0e7f277f20831a70607e31cd256f51974c59d529
Pilar84 <pilargijor@hotmail.es>
2025-10-27 12:25:30 +0100

    He modificado el archivo data.py Comp2

--- data.py
+++ data.py
@@ -3,4 +3,5 @@ numeros = [10, 20, 30, 40, 50]
 
 def mostrar_datos():
     for n in numeros:
-        print("Número:", n)
+        print(f"[Comp1] Numero actual -> {n}")
+    print ("Fin de la lista de numeros (Comp1)")
