d454edc2bef01b6340f940d1bc87e36318b0cda2
FernandoDM-1619 <fernandodiezmunoz@gmail.com>
2025-10-27 09:00:02 +0100

    Cambio nuevo ejercicio 13

--- data.py
+++ data.py
@@ -1,7 +1,8 @@
+
 # Datos de ejemplo para practicar con el repo
 numeros = [10, 20, 30, 40, 50]
 
 def mostrar_datos():
     for n in numeros:
         print("Número:", n)
-print("nuevo ej13")
+print("nuevo ejercicio 13")
