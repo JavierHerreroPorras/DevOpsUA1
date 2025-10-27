1cdcd8115d1d66097f3aada2fc3cb36e009df405
raulgomezg5 <gomezgarciar4@alumnos.ilerna.com>
2025-10-27 09:10:53 +0100

    Implementar saludo personalizado

--- app.py
+++ app.py
@@ -8,3 +8,13 @@ print("Hola")
 print("Nueva línea sin commit")
 
 print("Hola, soy Raul Gomez")
+
+
+# app.py
+
+def saludo_personalizado():
+    nombre = input("¡Hola! ¿Cómo te llamas? ")
+    print(f"¡Encantado de conocerte, {nombre}!")
+
+if __name__ == "__main__":
+    saludo_personalizado()
