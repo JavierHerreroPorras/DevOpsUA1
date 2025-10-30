a6ec82f0958560d6f4208277bb7461d87b0b3ce6
Pilar84 <pilargijor@hotmail.es>
2025-10-30 13:25:15 +0100

    chore(data): añadir docstring y mostrar posición de números

--- data.py
+++ data.py
@@ -1,3 +1,11 @@
+  GNU nano 8.5                                                 data.py
+# Datos de ejemplo para practicar con el repo
+numeros = [10, 20, 30, 40, 50]
+
+def mostrar_datos():
+    for n in numeros:
+        print("Número:", n)
+
 # Datos de ejemplo para practicar con el repo
 numeros = [10, 20, 30, 40, 50]
 
