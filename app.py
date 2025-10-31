15f2da2cde28994d974e348e27094a2139db5e70
nicosimonmilano <nicosimonmilano@alumnos.ilerna.com>
2025-10-31 12:27:00 +0100

    commit desde mi rama del fork

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 print("Mejora ejercicio 12")
 print("Modificacion ejercicio 14") 
+
+from datetime import datetime
+print("modificacion de fecha:")
+ahora = datetime.now()
+print(ahora.strftime("%d/%m/%Y %H:%M:%S"))
+print("modificacion de fecha hecha")
