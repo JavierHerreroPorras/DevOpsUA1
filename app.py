9e18eb9eea54fa4756e8d439e5ebfa5b2c651604
Rocío López <rociolopezmunoz04@alumnos.ilerna.com>
2025-10-31 14:22:28 +0100

    Ejercicio24

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 print("Mejora ejercicio 12")
 print("Modificacion ejercicio 14") 
 print("Modificacion ejercicio 18")
+print("Hola")
 
 def saludar(nombre):
 	retunr f"Hol, {nombre}!"
 
 def despedida():
     print("Gracias por usar la aplicación, ¡hasta pronto!")
+
