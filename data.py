bed94f85a7c58816796ba28804be4c43bf9168d3
Pilar84 <pilargijor@hotmail.es>
2025-10-31 08:31:06 +0100

    Mejora: elimina código duplicado y añade docstring a mostrar_datos

--- data.py
+++ data.py
@@ -1,14 +1,10 @@
-  GNU nano 8.5                                                 data.py
-# Datos de ejemplo para practicar con el repo
-numeros = [10, 20, 30, 40, 50]
+  GNU nano 8.5                                      
 
-def mostrar_datos():
-    for n in numeros:
-        print("Número:", n)
 
 # Datos de ejemplo para practicar con el repo
 numeros = [10, 20, 30, 40, 50]
 
 def mostrar_datos():
+    """Muestra cada número de la lista 'numeros'."""
     for n in numeros:
         print("Número:", n)
