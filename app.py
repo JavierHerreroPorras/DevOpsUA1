2f642d6d78f8edbed4ac8fe961907a61f7043fb3
DarioLC04 <dariolopezcastillejo@gmail.com>
2025-11-01 21:26:50 +0100

    He añadido nueva linea de codigo

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
 print("Mejora ejercicio 12")
 print("Modificacion ejercicio 14") 
+print("pls teacher no de git la semana que viene")
+print("Este ejercicio es peor que las rankeds del league of legends")
