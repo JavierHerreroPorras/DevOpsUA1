69f58f5ad0a92693c8631e2a0b6e1d65396999e5
Rocío López <rociolopezmunoz04@alumnos.ilerna.com>
2025-11-02 18:35:44 +0100

    feat(add): Añadir mensaje de despedida

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def saludar(nombre):
 	retunr f"Hol, {nombre}!"
 
 def despedida():
-    print("Gracias por usar la aplicación, ¡hasta pronto!")
+    print("Gracias por usar la aplicación, hasta pronto!!.")
 
